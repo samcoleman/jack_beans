7379774b0d273d3eee037178d05c6a802ccc2ea6
samcoleman <samuelconlancoleman@hotmail.co.uk>
2023-03-14 15:32:40 +0000

    updated proxy to include websockets, fast refresh

--- APP/proxy.js
+++ APP/proxy.js
@@ -3,7 +3,7 @@ var httpProxy = require('http-proxy');
 var https = require('https');
 
 
-httpProxy.createServer({
+var server = httpProxy.createServer({
     target: {
       host: 'localhost',
       port: 3000
@@ -11,7 +11,14 @@ httpProxy.createServer({
     ssl: {
       key: fs.readFileSync('.certs/localhost-key.pem', 'utf8'),
       cert: fs.readFileSync('.certs/localhost.pem', 'utf8')
-    }
-  }).listen(3001);
+    },
+    ws : true
+  })
+
+server.on('upgrade', function (req, socket, head) {
+    proxy.ws(req, socket, head);
+});
+
+server.listen(3001);
 
 console.log("Created https proxy on https://localhost:3001 from http://localhost:3000");
